823b093f29f1f8e415cb6a5f2d3b9ee27e7a76ef
ellie <ellie.khalid1@outlook.com>
2026-03-21 23:00:36 +0000

    background smachground
    mmmmmmm

--- src/components/BlogTitle.tsx
+++ src/components/BlogTitle.tsx
@@ -3,10 +3,12 @@ import React, {forwardRef, ForwardedRef} from 'react';
 const BlogTitle = forwardRef((props: unknown, ref: ForwardedRef<HTMLTextAreaElement>) => {
     return (
         <div className = "blog-title-container section">
+          <div className = "blog-title-container-inner">
             <h1>Blog Title</h1>
             <textarea
                 ref={ref}
                 placeholder='Enter your blog title here...' />
+          </div>
         </div>
     );
 });
